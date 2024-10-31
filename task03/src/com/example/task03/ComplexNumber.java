9ae6572bd8d4a99b1a9198916489e1173fd2934a
Andrey <eldotefc@gmail.com>
2024-10-31 19:11:49 +0500

    task 3

---
+++ task03/src/com/example/task03/ComplexNumber.java
@@ -0,0 +1,42 @@
+package com.example.task03;
+
+import java.awt.font.TextHitInfo;
+
+public class ComplexNumber {
+    private final double real;
+    private final double imaginary;
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber Sum(ComplexNumber number) {
+        double realSum = this.real + number.real;
+        double imaginarySum = this.imaginary + number.imaginary;
+        return new ComplexNumber(realSum, imaginarySum);
+    }
+
+    public ComplexNumber Product(ComplexNumber number) {
+        double realProduct = this.real * number.real - this.imaginary * number.imaginary;
+        double imaginaryProduct = this.real * number.imaginary + this.imaginary * number.real;
+        return new ComplexNumber(realProduct, imaginaryProduct);
+    }
+
+    @Override
+    public String toString() {
+        if (imaginary < 0) {
+            return String.format("%.2f-%.2fi", real, -imaginary);
+        } else {
+            return String.format("%.2f+%.2fi", real, imaginary);
+        }
+    }
+}
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,17 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(3, 4);
+        ComplexNumber num2 = new ComplexNumber(1, -2);
 
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+
+        ComplexNumber sum = num1.Sum(num2);
+        System.out.println("Sum: " + sum);
+
+        ComplexNumber product = num1.Product(num2);
+        System.out.println("Product: " + product);
     }
 }
+
